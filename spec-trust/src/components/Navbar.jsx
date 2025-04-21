8095c886aa2625801f7abbfa6f55c48a677fbbd6
Jagoda <jagoda.trzeciak@ddg.bio>
2025-04-21 08:22:16 +0200

    readme-- exemplary datasets

--- spec-trust/src/components/Navbar.jsx
+++ spec-trust/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 export default function Navbar() {
     return (
-        <nav className={"navbar navbar-light bg-light d-flex justify-content-between px-3"}>
+        <nav className="navbar navbar-light bg-light d-flex justify-content-between px-3">
             <a className="navbar-brand d-flex align-items-center">
                 <img src="/spectrometer.png" width="30" height="30"
                      className="d-inline-block align-top me-2 ms-2" alt=""/>
                 SpecTrust
             </a>
+
+            <a href="https://github.com/jagodatrzeciak/spec-trust/blob/main/README.md" target="_blank" rel="noopener noreferrer" className="btn btn-outline-secondary">
+                README
+            </a>
         </nav>
     );
 }
+
